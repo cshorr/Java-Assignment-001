397b6ee070c3fb4c215ab7ac6d20f433cec6d163
Cshor <cshortt707@gmail.com>
2024-12-03 20:32:19 -0800

    JustPracticing!

--- src/AlanTuring.java
+++ src/AlanTuring.java
@@ -1,9 +1,11 @@
 public class AlanTuring {
     /**
+     * @author chris Shortt
      * Prints Alan Turing to the console
      * @param args Command line arguments [The source file path, The target file path, ...]
      **/
     public static void main(String[] args) {
         // Write your program here
+        System.out.println("Alan Turing");
     }
 }
--- src/InAGalaxyFarFarAway.java
+++ src/InAGalaxyFarFarAway.java
@@ -1,9 +1,11 @@
 public class InAGalaxyFarFarAway {
     /**
+     * @author chris Shortt
      * Prints a famous Star Wars intro to the console with 3 souts.
      * @param args Command line arguments [The source file path, The target file path, ...]
      **/
     public static void main(String[] args) {
         // Write your program here
+        System.out.println("A long time ago\nin a galaxy far,\nfar away....");
     }
 }
